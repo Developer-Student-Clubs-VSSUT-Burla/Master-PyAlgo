08ccec29dce681dfb5d27757cb913966767799ee
AITIK DANDAPAT <aitikdandapat@gmail.com>
2021-02-03 22:21:57 +0530

    Solved Reverse Array

---
+++ Topic Wise Questions with Solutions/Array/reverse_array.py
@@ -0,0 +1,23 @@
+# Given an array (or string), the task is to reverse the array/string.
+# Examples : 
+ 
+
+# Input  : arr[] = {1, 2, 3}
+# Output : arr[] = {3, 2, 1}
+
+# Input :  arr[] = {4, 5, 1, 2}
+# Output : arr[] = {2, 1, 5, 4}
+
+
+def reverseList(A, start, end):
+	while start < end:
+		A[start], A[end] = A[end], A[start]
+		start += 1
+		end -= 1
+
+# Driver function to test above function
+A = list(map(int,input().split("")))
+print(A)
+reverseList(A, 0, len(A))
+print("Reversed list is")
+print(A)
