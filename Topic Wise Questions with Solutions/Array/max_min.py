2f7285998ad420621503fde896a875c4258ca7e9
Vaishnavi <vaishshells@gmail.com>
2021-03-10 15:36:03 +0530

    Modified

--- Topic Wise Questions with Solutions/Array/max_min.py
+++ Topic Wise Questions with Solutions/Array/max_min.py
@@ -1,5 +1,15 @@
 #enter elements of array
 elements = list(map(int,input("enter elements of array: ").split()))
 
-print("maximum: ", max(elements)," ","minimum: ",min(elements))
+mini=100000
+maxi=0
+
+for a in elements:
+	if (a<mini):
+		mini=a
+	if(a>maxi):
+		maxi=a
+
+print("Maximum Element is : ",maxi)
+print("Minimum Element is : ",mini)
 
