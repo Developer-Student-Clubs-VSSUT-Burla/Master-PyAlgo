94c8b9f7d728c57a8b8407ac1057c2829535eb47
Javeed Suhail <javeed3157@gmail.com>
2021-03-17 08:02:11 +0530

    Added Linear Search and element index #93

--- Search/Linear_Search.py
+++ Search/Linear_Search.py
@@ -1,20 +1,20 @@
 """
 Take an array as input and the element to search
 Search for the element by iterating through each element in array and 
-return the corresponding position of element if it is found .
+return the corresponding index of element if it is found .
 """
 
 def linearsearch(arr,ele):
     for i in range(len(arr)):
         if(arr[i]==ele):
-            return i+1
+            return i
     return -1
 
 array=list(map(int,input('Input some numbers : ').split()))
 element=int(input('Enter element to search : '))
 a=linearsearch(array,element)
 if(a>0):
-    print('Element is found at position ',a)
+    print('Element is found at index ',a)
 else:
     print('Element is not present in array')
 
@@ -24,7 +24,7 @@ else:
 # TestCase 1:
 # Input some numbers : 5 4 1 3 2
 # Enter element to search : 3
-# Element is found at 4
+# Element is found at 3
 
 # TestCase 2:
 # Input some numbers : 2 8 4 6 1 6 3 5 2
