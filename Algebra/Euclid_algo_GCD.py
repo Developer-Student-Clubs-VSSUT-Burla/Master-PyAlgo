715d2f67dceb15d0f6de547d57c94bcca1ffdb6f
Tanvi Wakade <tanvi.wakade19@vit.edu>
2021-03-20 23:53:48 +0530

    Euclidian Algorithm for finding GCD

---
+++ Algebra/Euclid_algo_GCD.py
@@ -0,0 +1,14 @@
+# Implementation of Euclid Algorithm to find GCD in Python3
+
+def gcd(a, b):
+    if a == 0:
+        return b
+
+    return gcd(b % a, a)
+
+
+print("\nEnter two numbers to find their GCD: ", end=" ")
+num1, num2 = [int(i) for i in input().split()]
+
+gcdNum = gcd(num1, num2)
+print("\nGCD of", num1, "and", num2, "is:", gcdNum)
