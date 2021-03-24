ae836445460766db9051a48cfca3de67d608be04
NEERAJ ADITYANANTH POLAMPALLI <65017645+NEERAJAP2001@users.noreply.github.com>
2021-03-24 21:02:16 +0530

    Update Trailing_Zeroes.py

--- Numerical/Trailing_Zeroes.py
+++ Numerical/Trailing_Zeroes.py
@@ -10,10 +10,10 @@ number = int(input('Enter Number for calculating trailing Zeroes : '))
 solution = trailingZeroes(number)
 print(solution)
 
-# Input: number = 3
-# Output: 0
-# Explanation: 3! = 6, no trailing zero.
+'''Input: number = 3
+Output: 0
+Explanation: 3! = 6, no trailing zero.
 
-# Input: number = 5
-# Output: 1
-# Explanation: 5! = 120
+Input: number = 5
+Output: 1
+Explanation: 5! = 120'''
