76e686112f0641a3ce40d12f7298e0ef6b3de490
Crazy-C0der <sh2000.cool.j@gmail.com>
2021-03-30 07:51:16 +0530

    necessary comments added

--- Algebra/Discrete_Logarithm.py
+++ Algebra/Discrete_Logarithm.py
@@ -1,9 +1,13 @@
 # discrete logarithm
-# a^k ≡ b(mod m)a
+# If a,b,m are given then among the numbers 0,1,2....,phi(n) (phi(n) is the totient function)
+# exactly one number 'k' such that a^k ≡ b(mod m), where b and m are relatively prime.
+# Then 'k' is called the discrete logarithm of b 
 
 import math
 from sympy.ntheory.factor_ import totient 
-  
+
+# Function DiscreteLogarithm() checks if 'k' exists for the given values of a,b,m 
+# -1 is returned if such value doesn't exist 
 def DiscreteLogarithm(a,b,m):
     b = b % m
     for i in range (totient(m)):
@@ -13,10 +17,11 @@ def DiscreteLogarithm(a,b,m):
     return -1
 
 print("Enter value for:")
-# Sample values a=45, b=78, m=89    
-# Sample output: k=3
 a = int(input("a:"))   
 b = int(input("b:"))   
 m = int(input("m:"))   
 print(DiscreteLogarithm(a,b,m))
 
+''' Sample input:  a=45, b=78, m=89    
+    output - value of 'k' that satisfies a^k ≡ b(mod m)
+           =  3 '''
