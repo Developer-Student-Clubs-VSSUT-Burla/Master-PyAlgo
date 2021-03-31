34e5558095a52c9f81fb6ed497e9ca42bce79881
JASKAMAL SINGH <54163864+JASKAMAL22@users.noreply.github.com>
2021-03-31 03:43:31 -0700

    Create Staircase.py

---
+++ Topic Wise Questions with Solutions/Array/Staircase.py
@@ -0,0 +1,33 @@
+'''
+A staircase of base and height both equal to n ,is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+
+Write a program that prints a staircase of size n.
+'''
+
+import os
+import random
+import re
+import sys
+
+# Complete the staircase function below.
+def staircase(n):
+    for i in range(n):
+        for j in range(n):                 
+            if j>=n-i-1:          # print "#" where j>n-i-1 otherwise space will be printed
+                print("#",end="")
+            else:
+                print(" ",end="")
+        print()                   # transfer the cursor to the next line
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    staircase(n)
+'''
+Ex:-  n=4
+   #
+  ##
+ ###
+####  
+'''
