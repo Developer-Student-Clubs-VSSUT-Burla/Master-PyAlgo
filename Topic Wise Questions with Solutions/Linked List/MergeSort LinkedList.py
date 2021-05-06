9943775541710afa18428d324a17d580f7b475e0
Prerna-07 <chitranshcse@gmail.com>
2021-05-06 12:41:49 +0530

    Update MergeSort LinkedList.py

--- Topic Wise Questions with Solutions/Linked List/MergeSort LinkedList.py
+++ Topic Wise Questions with Solutions/Linked List/MergeSort LinkedList.py
@@ -1,20 +1,17 @@
-class Node:
+class ConstructNode:
     def __init__(self, data):
         self.data = data
         self.next = None
         
-class LinkedList:
+class LLConstruct:
     def __init__(self):
         self.head = None
-     
-    # push new value to linked list
-    # using append method
-    def append(self, new_value):
+    def appendatlast(self,data):
          
-        # Allocate new node
-        new_node = Node(new_value)
+        
+        new_node = ConstructNode(data)
          
-        # if head is None, initialize it to new node
+       
         if self.head is None:
             self.head = new_node
             return
@@ -22,50 +19,48 @@ class LinkedList:
         while curr_node.next is not None:
             curr_node = curr_node.next
              
-        # Append the new node at the end
-        # of the linked list
+       
         curr_node.next = new_node
     
-    def sortedMerge(self, a, b):
+    def Merge(self, list1, list2):
         result = None
          
-        # Base cases
-        if a == None:
-            return b
-        if b == None:
-            return a
+        
+        if list1 == None:
+            return list2
+        if list2 == None:
+            return list1
              
-        # pick either a or b and recur..
-        if a.data <= b.data:
-            result = a
-            result.next = self.sortedMerge(a.next, b)
+        
+        if list1.data <= list2.data:
+            result = list1
+            result.next = self.Merge(list1.next, list2)
         else:
-            result = b
-            result.next = self.sortedMerge(a, b.next)
+            result = list2
+            result.next = self.Merge(list1, list2.next)
         return result
 
-    def mergeSort(self, h):
-        if h == None or h.next == None:
-            return h
+    def mergeSort(self, start):
+        if start == None or start.next == None:
+            return start
  
-        # get the middle of the list
-        middle = self.getMiddle(h)
+        
+        middle = self.getMiddle(start)
         nexttomiddle = middle.next
  
-        # set the next of middle node to None
+        
         middle.next = None
  
-        # Apply mergeSort on left list
-        left = self.mergeSort(h)
+       
+        left = self.mergeSort(start)
          
-        # Apply mergeSort on right list
+        
         right = self.mergeSort(nexttomiddle)
  
-        # Merge the left and right lists
-        sortedlist = self.sortedMerge(left, right)
+       
+        sortedlist = self.Merge(left, right)
         return sortedlist 
-    # Utility function to get the middle
-    # of the linked list
+   
   
     def getMiddle(self, head):
         if (head == None):
@@ -74,34 +69,31 @@ class LinkedList:
         slow = head
         fast = head
  
-        while (fast.next != None and
-               fast.next.next != None):
+        while (fast.next != None and fast.next.next != None):
             slow = slow.next
             fast = fast.next.next
              
         return slow       
 
-def printList(head):
+def Display(head):
     if head is None:
         print(' ')
         return
     curr_node = head
     while curr_node:
-        print(curr_node.data, end = " ")
+        print(curr_node.data, end = " -> ")
         curr_node = curr_node.next
-    print(' ')
+    print('.')
      
-# Driver Code
+
 if __name__ == '__main__':
-    li = LinkedList()
+    ll = LLConstruct()
      
     n = int(input());
     Elements = list(map(int,input().split()));
     
     for i in range (n):
-        li.append(Elements[i]);
+        ll.appendatlast(Elements[i]);
      
-    # Apply merge Sort
-    li.head = li.mergeSort(li.head)
-    print ("Sorted Linked List is:")
-    printList(li.head)
+    ll.head = ll.mergeSort(ll.head)
+    Display(ll.head)
