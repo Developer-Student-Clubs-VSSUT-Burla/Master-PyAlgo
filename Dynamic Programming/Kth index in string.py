f49d0fe4027a4498c9186b7593b904122cc42e2f
Khushi agarwal <khushiagarwal@MacBook-Air.local>
2021-05-15 16:37:14 +0530

    changes

--- Dynamic Programming/Kth index in string.py
+++ Dynamic Programming/Kth index in string.py
@@ -4,21 +4,21 @@
  '''
 
  #main function
- def main():
-     s=input("enter a string")
-     k=int(input("enter the index"))
-     l=len(s)
- 	#Check whether the value of k is greater than length
-     if(k>l):
-         print(s)
-     #If k is less than length of string then remove the kth index value
-     else:
-          s1=''
-          for i in range(0,l):
-              if(i!=k):
-                  s1=s1+s[i]
-          print(s1)
- if __name__== "__main__":
+def main():
+    s=input("enter a string")
+    k=int(input("enter the index"))
+    l=len(s)
+    #Check whether the value of k is greater than length
+    if(k>l):
+        print(s)
+    #If k is less than length of string then remove the kth index value
+    else:
+        s1=''
+        for i in range(0,l):
+            if(i!=k):
+                s1=s1+s[i]
+        print(s1)
+if __name__== "__main__":
      main()
 
  '''
@@ -30,4 +30,6 @@
  enter the index 2
  pyhon
  '''
+
+
  
